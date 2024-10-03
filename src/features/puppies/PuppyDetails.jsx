5a28931d4460f80fca7b70f94c9f93b3d3b3a7e9
Brandon Hamilton <bham2015@gmail.com>
2024-10-02 22:18:45 -0500

    Added the skip option in useGetPuppyQuery in PuppyDetails.jsx to avoid the 500 server error.

--- src/features/puppies/PuppyDetails.jsx
+++ src/features/puppies/PuppyDetails.jsx
@@ -7,14 +7,15 @@ import { useDeletePuppyMutation, useGetPuppyQuery } from "./puppySlice";
  */
 export default function PuppyDetails({ selectedPuppyId, setSelectedPuppyId }) {
   // TODO: Grab data from the `getPuppy` query
-  const { data: puppy, isLoading } = useGetPuppyQuery(selectedPuppyId);
+  const { data: puppy, isLoading } = useGetPuppyQuery(selectedPuppyId, {
+    skip: !selectedPuppyId,
+  });
 
   // TODO: Use the `deletePuppy` mutation to remove a puppy when the button is clicked
   const [deletePuppy] = useDeletePuppyMutation();
 
   function removePuppy(id) {
     setSelectedPuppyId();
-    console.log("Adopting Doge by id", id);
     deletePuppy(id);
   }
 
